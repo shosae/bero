042d2bd085c3d561e512c74ec03c05a49b085d05
shosae <phone13324@gmail.com>
2025-09-25 18:40:36 +0900

    fix: 🐛 resolve flake8 test failures - handle E501 with #noqa

--- src/bero_description/setup.py
+++ src/bero_description/setup.py
@@ -12,7 +12,7 @@ setup(
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         (os.path.join('share', package_name, 'urdf'), glob(os.path.join('urdf', '*.urdf'))),
-        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*.launch.py'))),
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*.launch.py'))),  # noqa: E501
     ],
     install_requires=['setuptools'],
     zip_safe=True,
